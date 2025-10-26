ee05bc51ffe251115993dd66cc8be73b8dfcad51
Tomoya-Kouno-Laboratory <kouno.tomoya.528@s.kyushu-u.ac.jp>
2025-10-26 16:42:14 +0900

    Revert "add for subtask from joint values"
    This reverts commit efa4e02d33fc1d5477d7ea7baf6c03f298a66db1.

--- launch/tms_if_for_opera.launch.py
+++ launch/tms_if_for_opera.launch.py
@@ -58,17 +58,15 @@ def generate_launch_description():
             {'collision_object_dump_record_name': LaunchConfiguration('collision_object_dump_record_name')},
             {'use_sim_time': LaunchConfiguration('use_sim_time')}
     ])
-    excavator_change_pose_from_joint_values_node_zx200 = Node(
+    scene_manager_node_zx200 = Node(
         package='tms_if_for_opera',
-        executable='excavator_change_pose_from_joint_values',
+        executable='scene_manager',
         namespace='zx200',
         parameters=[
-            {'robot_description': robot_description_content},
             {'planning_group': LaunchConfiguration('planning_group')},
-            {'collision_object_record_name': LaunchConfiguration('collision_object_record_name')},
-            {'collision_object_dump_record_name': LaunchConfiguration('collision_object_dump_record_name')},
+            {'other_robots_config': LaunchConfiguration('other_robots_config')},
             {'use_sim_time': LaunchConfiguration('use_sim_time')}
-    ])
+        ])
     
     excavator_navigate_through_poses_node_zx200 = Node(
         package='tms_if_for_opera',
@@ -200,7 +198,7 @@ def generate_launch_description():
         declare_other_robots_config_arg,
         
         excavator_change_pose_from_poses_node_zx200,
-        excavator_change_pose_from_joint_values_node_zx200,
+        scene_manager_node_zx200,
         excavator_navigate_through_poses_node_zx200,
         excavator_navigate_anywhere_node_zx200,
         excavator_follow_waypoints_node_zx200,
